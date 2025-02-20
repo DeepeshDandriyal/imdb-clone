a567089b16ca00ada4b92ec73b0dc530df9ca6c9
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2025-02-20 16:04:00 +0530

    add dark mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-
+import ThemeCom from "@/components/ThemeCom";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,8 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <ThemeCom>
+          <Header />
+          {children}
+        </ThemeCom>
       </body>
     </html>
   );
